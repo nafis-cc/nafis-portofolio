98cc0fd500ad95b6709411cf41e9bc324c5ee5c6
Muhammad Nafis <m.nafis.2001@gmail.com>
2026-08-13 11:44:46 +0700

    mod: edit clients

--- src/views/shared/components/aside.jsx
+++ src/views/shared/components/aside.jsx
@@ -3,10 +3,10 @@ import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { TfiLinkedin } from "react-icons/tfi";
 import contacts from "~/models/contacts.json";
-import { handleOpenLinkedin } from "~/scripts/app";
+import { handleOpenNafisPortofolio } from "~/scripts/app";
 import { initSidebarToggle } from "~/scripts/app";
 
-const Profile = new URL("/images/nafis.jpg", import.meta.url).href;
+const Profile = new URL("/images/nafis.png", import.meta.url).href;
 
 // simpan komponen, bukan elemen
 const iconMap = {
@@ -30,7 +30,7 @@ function Aside({ ICON_SIZE = 18 }) {
           <img src={Profile} alt="" width="80" />
         </figure>
 
-        <button onClick={handleOpenLinkedin} className="info-content">
+        <button onClick={handleOpenNafisPortofolio} className="info-content">
           <h1 className="name">Muhammad Nafis</h1>
           <p className="title">IT Digital Consultant</p>
         </button>
